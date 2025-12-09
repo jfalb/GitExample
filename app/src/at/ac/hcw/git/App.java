b3189d8b540bc38ede8c919affb5cb33f9ce9071
Juergen Falb <j.falb@nousdigital.net>
2025-12-09 19:46:12 +0100

    Added welcome message

--- app/src/at/ac/hcw/git/App.java
+++ app/src/at/ac/hcw/git/App.java
@@ -2,6 +2,7 @@ package at.ac.hcw.git;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Welcome to Git!");
         System.out.println("Hello World!");
     }
 }
