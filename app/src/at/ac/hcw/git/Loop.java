51b2769c6753cfb3a8138ca917ff938a0e2da178
Juergen Falb <j.falb@nousdigital.net>
2025-12-09 20:06:02 +0100

    changed loop

---
+++ app/src/at/ac/hcw/git/Loop.java
@@ -0,0 +1,10 @@
+package at.ac.hcw.git;
+
+public class Loop {
+
+    public void loop() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("number " + i);
+        }
+    }
+}
