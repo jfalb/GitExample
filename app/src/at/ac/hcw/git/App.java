9b6d0fd3627cbf3b4b8844fe58b1677f7dac5d82
Juergen Falb <j.falb@nousdigital.net>
2025-12-09 19:47:48 +0100

    Added loop to print

--- app/src/at/ac/hcw/git/App.java
+++ app/src/at/ac/hcw/git/App.java
@@ -3,5 +3,9 @@ package at.ac.hcw.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
